34d7c805980c1bb20c7939b436ac5f86a42e96ef
d-yeon <147508048+d-yeon@users.noreply.github.com>
2023-10-19 02:08:49 +0900

    Add files via upload

---
+++ 01. 파이썬 시작하기/001 ~ 010.py
@@ -0,0 +1,34 @@
+# 001 print 기초
+print("Hello World")
+
+# 002 print 기초
+print("Mary's cosmetics")
+
+# 003 print 기초
+print('신씨가 소리질렀다. "도둑이야".')
+
+# 004 print 기초
+print("C:\Windows")
+
+# 005 print 탭과 줄바꿈
+print("안녕하세요.\n만나서\t\t반갑습니다.")
+### \n 은 줄바꿈을 의미
+### \t 는 탭을 의미
+
+# 006 print 여러 데이터 출력
+print("오늘은", "일요일")
+### 결과 = 오늘은 일요일
+
+# 007 print 기초
+## naver;kakao;sk;samsung
+print("naver", "kakao", "samsung", sep=";")
+
+# 008 print 기초
+## naver/kakao/sk/samsung
+print("naver", "kakao", "samsung", sep="/")
+
+# 009 print 줄바꿈
+print("first", end="");print("second")
+
+# 010 연산 결과 출력
+print(5/3)  
